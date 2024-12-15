988d1f70e517fcb6e6fd2c93d1aea1e768a05d71
Albert <amallad2@xtec.cat>
2024-12-15 23:19:00 +0100

    prova1

--- prova.py
+++ prova.py
@@ -10,5 +10,7 @@ alphabet = string.ascii_letters + string.digits  # a-z, A-Z, 0-9
 # Generar un token de 16 caracteres (puedes ajustar el tamaño)
 token = ''.join(secrets.choice(alphabet) for _ in range(64))
 print(token)
+print(token)
+print(token)
 
 
